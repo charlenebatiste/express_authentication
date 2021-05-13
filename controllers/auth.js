7cd3dde434ec91113031c6e277e62831057cee60
charlenebatiste <batistecharlene@gmail.com>
2021-05-13 16:00:05 -0400

    auth: add login post route

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,8 +6,23 @@ router.get('/signup', (req, res) => {
   res.render('auth/signup');
 });
 
+
+// GET ROUTE TO RENDER LOGIN PAGE
+
 router.get('/login', (req, res) => {
   res.render('auth/login');
 });
+
+// POST ROUTE
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/auth/login',
+  successFlash: 'Welcome back ...',
+  failureFlash: 'Either email or password is incorrect' 
+}));
+
+
+
 
 module.exports = router;
